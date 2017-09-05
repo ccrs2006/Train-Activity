b9e94da193100de220e1c62609c17dc30357c12d
Robert Castiblanco <ccrs2006@gmail.com>
2017-09-04 23:04:20 -0600

    added a remove button and also added sound effect

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,100 +1,98 @@
+  $(document).ready(function() {
+
   // --------------Initialize Firebase
   var config = {
     apiKey: "AIzaSyDyS8_RgF9olUnXG4NgCfk42Qf45MJ76d0",
     authDomain: "train-activity-ea976.firebaseapp.com",
     databaseURL: "https://train-activity-ea976.firebaseio.com",
     projectId: "train-activity-ea976",
-    storageBucket: "",
+    storageBucket: "train-activity-ea976.appspot.com",
     messagingSenderId: "137896900180"
   };
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
-  var currentTime = moment().toDate().getTime();
-      //this puts user's current time on main page
-      $(".current-time").append(moment(currentTime).format("MM/DD/YY hh:mm:ss"));
+  var currentTime = moment();
 
-  // 2. Button for adding Train
+  //this puts user's current date and time on the main page
+  $(".current-time").append(moment(currentTime).format("MM/DD/YY hh:mm:ss"));
+
+  // Button for adding Train
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
-  // Grabs user input
+
   var trainName = $("#train-name-input").val().trim();
   var destination = $("#destination-input").val().trim();
   var firstTrain = $("#first-train-time-input").val().trim(); 
   var frequency = $("#frequency-input").val().trim();
-  var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-  var currentTime = moment();
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % frequency;
-  var minutesTillTrain = frequency - tRemainder;
-  var NextArrival = moment().add(minutesTillTrain, "minutes");
-  var minutesAway = moment(NextArrival).format("hh:mm");
-  // Creates local "temporary" object for holding Train data
+  var startTime = moment(firstTrain, "hh:mm").subtract(1, "years");
+  var minutesAway = moment().diff(moment(startTime), "minutes") % frequency;
+  var nextArrival = moment(moment().add(minutesAway, "minutes")).format("hh:mm");
+  var minutesTillTrain = moment().diff(moment(startTime), "minutes") % frequency;
+
+  // "temporary" object for holding Train data
   var newTrain = {
     trainName: trainName,
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency,
-    minutesAway: minutesAway,
+    nextArrival: nextArrival,
     minutesTillTrain: minutesTillTrain
   };
   // Uploads Train data to the database`
   database.ref().push(newTrain);
   // Logs everything to console
-  console.log(newTrain.trainName);
-  console.log(newTrain.destination);
-  console.log(newTrain.firstTrain);
-  console.log(newTrain.frequency);
-  console.log(minutesAway);
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrain);
+  console.log(frequency);
+  console.log(nextArrival);
   console.log(minutesTillTrain);
+  //add sound every time a train is added
+   var $audioCharacter = document.createElement('audio');
+                    $audioCharacter.setAttribute('src', 'assets/sounds/trainSound.mp3');
+                    $audioCharacter.play();
   // Alert
-  alert("train successfully added");
+  // alert("Train successfully added");
   // Clears all of the text-boxes
   $("#train-name-input").val("");
   $("#destination-input").val("");
   $("#first-train-time-input").val("");
   $("#frequency-input").val("");
+
+  return false;
+
 });
+
   // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-    console.log(childSnapshot.val());
-  // Store everything into a variable.
-  var trainName = childSnapshot.val().trainName;
-  var destination = childSnapshot.val().destination;
-  var firstTrain = childSnapshot.val().firstTrain;
-  var frequency = childSnapshot.val().frequency;
-  // Train Info
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTrain);
-  console.log(frequency);
-  console.log(minutesAway);
-  console.log(minutesTillTrain);
-
-  // calculates next arrival
-  var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
-  var currentTime = moment();
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  var tRemainder = diffTime % frequency;
-  var minutesTillTrain = frequency - tRemainder;
-  var NextArrival = moment().add(minutesTillTrain, "minutes");
-  var minutesAway = moment(NextArrival).format("hh:mm");
-  //Calculates next train arrival time
-  console.log(minutesAway);
-
-  // Calculates minutes away
-  console.log(minutesTillTrain);
 
   // Add each train's data into the table
   $("#train-table").append("<tr><td>" 
-    + trainName + "</td><td>" 
-    + destination + "</td><td>" 
-    + frequency + "</td><td>"
-    + nextArrival + "</td><td>" 
-    + minutesAway + "</td></td>"
-    + '<input type ="submit" value ="remove train" class = "remove-train btn btn-primary btn-sm">'
-    + "</td>" + "</tr>"
+    + childSnapshot.val().trainName 
+    + "</td><td>" 
+    + childSnapshot.val().destination 
+    + "</td><td>" 
+    + childSnapshot.val().frequency 
+    + "</td><td>"
+    + childSnapshot.val().nextArrival 
+    + "</td><td>" 
+    + childSnapshot.val().minutesTillTrain 
+    + "</td><td>"
+    + "<button class='remove-train btn btn-danger' id='add-train-btn' type='submit'>Remove Train</button>"
+    + "</td></tr>"
     );
+  //Handles any errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
   });
+
+  $("body").on("click", ".remove-train", function(){
+      $(this).closest("tr").remove();
+      var getKey = $(this).parent().attr("id");
+      database.child(getKey).remove();
+  });
+
+});//Closes jQuery wrapper
   
